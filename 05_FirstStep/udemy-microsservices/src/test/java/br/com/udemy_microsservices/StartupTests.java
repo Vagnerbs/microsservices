ccf77eeb83922df8b2ed50badbf3aec9883c7489
Vagnerbs <vagnerb1987@gmail.com>
2025-01-06 23:31:46 -0300

    49.Criando o Primeiro Endpoint REST

---
+++ 05_FirstStep/udemy-microsservices/src/test/java/br/com/udemy_microsservices/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy_microsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
